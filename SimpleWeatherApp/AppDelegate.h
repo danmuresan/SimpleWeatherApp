f4e55f155c260c66e35374cce0fd4e77c8ada65e
Muresan, Dan-Sorin <muresand@fa-001417mac.local>
2017-03-06 12:47:43 +0200

    Initial Commit

---
+++ SimpleWeatherApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SimpleWeatherApp
+//
+//  Created by Muresan, Dan-Sorin on 3/6/17.
+//  Copyright © 2017 Muresan, Dan-Sorin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ SimpleWeatherApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleWeatherApp
+//
+//  Created by Muresan, Dan-Sorin on 3/6/17.
+//  Copyright © 2017 Muresan, Dan-Sorin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
